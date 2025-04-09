20a73001fc2b6e3f025bf74d9d1153201cde94f7
omaayma <zemmouriomayma@gmail.com>
2025-04-09 12:56:59 +0200

    Add files via upload

---
+++ ej 1.ps1
@@ -0,0 +1,28 @@
+﻿clear-host
+if($args.count -ne 3){
+write-host "Tienes que pasar 3 parámetros"
+}
+$opcion=read-host "Elige una opción"
+switch ($args.count){
+0 { get-content 
+write-host "mostar contenido del fichero"  
+}
+1 { 
+select-object -Property name,creationTime | Where-Object -eq $args[1]
+write-host "El nombre y la fecha de creación de este fichero"
+}
+2 { cd C:\
+get-childitem | where name -eq  $args[2]
+write-host "Ya existe un fichero con igual nombre"
+}
+default { write-host "Parámetro no válido"
+}
+}
+
+
+
+
+
+
+
+
---
+++ ej1.ps1
@@ -0,0 +1,29 @@
+﻿clear-host
+if($args.count -ne 3){
+write-host "Tienes que pasar 3 parámetros"
+}
+if ($args.count -eq 3) {
+$opcion=read-host "Introduzca la opcion"
+
+switch ($opcion){
+1 { get-content $args[0]
+}
+2 { 
+get-childitem | select-object -Property name,creationTime | Where-Object name -eq $args[1]
+
+}
+3 { get-childitem C:\| where name -eq  $args[2]
+
+}
+default { write-host "Parámetro no válido"
+}
+}
+}
+
+
+
+
+
+
+
+
